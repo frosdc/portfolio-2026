8e798129b1d8b028a68c3d5c55dd10fbf2d3c48d
frosdc <pscs0000@gmail.com>
2026-08-28 08:47:09 +0800

    making the images in art monochrome

--- src/components/Art.vue
+++ src/components/Art.vue
@@ -39,10 +39,10 @@ const hang = [
         <div class="mt-16 grid grid-cols-6 gap-gutter md:mt-32 md:grid-cols-12 md:gap-y-32">
             <figure v-for="(piece, i) in artPieces" :key="piece.id" class="col-span-6 m-0"
                 :class="hang[i % hang.length].span" :data-speed="hang[i % hang.length].speed">
-                <div class="group relative w-full overflow-hidden" :class="hang[i % hang.length].ratio"
+                <div class="group relative w-full overflow-hidden bg-[#ecebe7]" :class="hang[i % hang.length].ratio"
                     data-cursor="View" data-reveal>
                     <img v-if="piece.image" :src="piece.image" :alt="piece.caption"
-                        class="absolute inset-0 h-full w-full object-cover transition-transform duration-[900ms] ease-[cubic-bezier(0.16,1,0.3,1)] group-hover:scale-[1.04]" />
+                        class="absolute inset-0 h-full w-full object-cover transition-all duration-[900ms] ease-[cubic-bezier(0.16,1,0.3,1)] grayscale group-hover:grayscale-0 group-hover:scale-[1.04]" />
                     <div v-else
                         class="absolute inset-0 transition-transform duration-[900ms] ease-[cubic-bezier(0.16,1,0.3,1)] group-hover:scale-[1.04]"
                         :class="piece.gradient"></div>
